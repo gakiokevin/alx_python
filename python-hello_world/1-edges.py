14d357d153d0193f7d88abea6d419245450c8944
gakiokevin <gakiokevin5@gmail.com>
2023-07-21 01:16:19 +0300

    1. Copy - Cut - Paste

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,10 +1,8 @@
 word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 word_first_3 = word[0:3]
-print("First 3 letters: {}".format(word_first_3))
+print("First 3 letters: {f}".format(f = word_first_3))
 word_last_2 = word[-2:]
-print("Last 2 letters: {}".format(word_last_2))
+print("Last 2 letters: {l}".format(l = word_last_2))
 middle_word = word[1:-1]
-print("Middle word: {}".format(middle_word))
-
-print("kevin "+"macharia")
+print("Middle word: {m}".format(m = middle_word))
