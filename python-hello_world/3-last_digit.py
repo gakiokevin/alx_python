46c5873e1dee01c5568eb34b326db617f17a0a6d
kelvin Macharia <101465969+gakiokevin@users.noreply.github.com>
2023-07-21 14:28:38 +0300

    Update 3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -5,16 +5,16 @@ if number < 0:
    m = str(number)
    if m[-1] == 0:
       print("Last digit of {number} is {lastDigit} and is 0".format(number=number, lastDigit=int(m[-1])))
-  
    else:
        lastDigit = "-"+ m[-1]
-   print("Last digit of {number} is {lastDigit} and is less than 6 and not 0".format(number=number, lastDigit=int(lastDigit)))
+      lastDigit = int(lastDigit)
+   print("Last digit of {number} is {lastDigit} and is less than 6 and not 0".format(number=number, lastDigit=lastDigit))
 else:
    m = str(number)
    lastDigit = m[-1]
    if int(lastDigit) > 5:
-       print("Last digit of {m} is {lastDigit} and is greater than 5".format(m = number, lastDigit=int(lastDigit)))
+       print("Last digit of {m} is {lastDigit} and is greater than 5".format(m = number, lastDigit=lastDigit))
    elif int(lastDigit) == 0 :
-           print("Last digit of {m} is {lastDigit} and is 0".format(m = number, lastDigit=int(lastDigit)))
+           print("Last digit of {m} is {lastDigit} and is 0".format(m = number, lastDigit=lastDigit))
    else:
-       print("Last digit of {m} is {lastDigit} and is less than 6 and not 0".format(m = number, lastDigit=int(lastDigit)))
+       print("Last digit of {m} is {lastDigit} and is less than 6 and not 0".format(m = number, lastDigit=lastDigit))
