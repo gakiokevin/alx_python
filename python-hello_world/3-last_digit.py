c64b568997003544dd65b797efe3ce030fcb184c
gakiokevin <gakiokevin5@gmail.com>
2023-07-21 14:03:37 +0300

    A program that generate random numbers

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,14 +3,18 @@ number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 if number < 0:
    m = str(number)
-   lastDigit = "-"+ m[-1]
+   if m[-1] == 0:
+      print("Last digit of {number} is {lastDigit} and is 0".format(number=number, lastDigit=int(m[-1])))
+  
+   else:
+       lastDigit = "-"+ m[-1]
    print("Last digit of {number} is {lastDigit} and is less than 6 and not 0".format(number=number, lastDigit=int(lastDigit)))
 else:
    m = str(number)
    lastDigit = m[-1]
    if int(lastDigit) > 5:
-       print("Last digit of {m} is {lastDigit} and is greater than 5".format(m = m, lastDigit=int(lastDigit)))
+       print("Last digit of {m} is {lastDigit} and is greater than 5".format(m = number, lastDigit=int(lastDigit)))
    elif int(lastDigit) == 0 :
-           print("Last digit of {m} is {lastDigit} and is 0".format(m = m, lastDigit=int(lastDigit)))
+           print("Last digit of {m} is {lastDigit} and is 0".format(m = number, lastDigit=int(lastDigit)))
    else:
-       print("Last digit of {m} is {lastDigit} and is less than 6 and not 0".format(m = m, lastDigit=int(lastDigit)))
+       print("Last digit of {m} is {lastDigit} and is less than 6 and not 0".format(m = number, lastDigit=int(lastDigit)))
