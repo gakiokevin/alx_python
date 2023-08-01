79dc32e04449bf3ab9f603bf1ff6c8c4bef98adf
gakiokevin <gakiokevin5@gmail.com>
2023-08-01 16:50:33 +0300

    more returns

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -2,4 +2,3 @@ def multiple_returns(sentence):
    length = len(sentence)
    first_char = ('Length: {} - First character: {}'.format(length,sentence[0])) if length > 0 else None
    return first_char,
-print(multiple_returns(''))
