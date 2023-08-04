3ad945e7e0b133401de513bb7a9cb3539645d868
gakiokevin <gakiokevin5@gmail.com>
2023-08-04 17:08:43 +0300

    best score

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -5,6 +5,6 @@ def best_score(a_dictionary):
       max_value = max(a_dictionary.values())
       for key,value in a_dictionary.items():
          if value == max_value :
-            return 'Best : {}'.format(key)
+            return key
    except ValueError:
       return None
